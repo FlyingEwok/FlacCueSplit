3df380186fa714b2b8fa3ab434a5cf3de93c5d90
Nicholas <nicholas.garth1@gmail.com>
2021-09-22 02:01:09 -0400

    Got flaccuesplit working

--- flaccuesplit.sh
+++ flaccuesplit.sh
@@ -2,24 +2,44 @@
 
 echo "Starting flaccuesplit"
 
+path="$1"
+cd "$path"
+
 flaccuesplit ()
 {
-    cueFile=$1
-    flacFile=$2
-    shnsplit -f $cueFile -t %n-%t -o flac $flacFile
+    cueFile="$1"
+    flacFile="$2"
+    echo "Now Splitting Files"
+    shnsplit -f "$cueFile" -t %n-%t -o flac "$flacFile"
+    if [ $? -ne 0 ]; then
+        echo "Splitting Failed!"
+        exit 1
+    fi
 }
 
 splitflacTag ()
 {
-    cueFile=$1
-    cuetag $cueFile [0-9]*.flac
+    cueFile="$1"
+    cuetag "$cueFile" [0-9]*.flac
+    echo "Files Tagged"
 }
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cueFile=(*.cue)
+flacFile=(*.flac)
 
-cueFile="SCRIPT_DIR/Outlaws - Playin' To Win + Ghost Riders.cue"
-flacFile="SCRIPT_DIR/Outlaws - Playin' To Win + Ghost Riders.flac"
+if [ ${#cueFile[@]} -gt 1 ]; then 
+    echo -e "Multiple files found\nSelect one."
+    for INDEX in ${!cueFile[@]}; do 
+        echo "${INDEX}) ${cueFile[${INDEX}]}"
+    done
+    
+    read -p "Enter the number of the file you want: " usrSelection 
+    cueFile="${cueFile[${usrSelection}]}"
+else
+    cueFile="${cueFile[0]}"
+fi
 
-flaccuesplit cueFile flacFile
 
-splitflacTag cueFile
+flaccuesplit "$cueFile" "${flacFile[0]}"
+
+splitflacTag "$cueFile"
